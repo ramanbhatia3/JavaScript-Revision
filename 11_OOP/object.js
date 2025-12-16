50a78f46cbf87bbb2ce1a1bd83f4cb60fb4596ab
ramanbhatia3 <bhattiramandeep3105@gmail.com>
2025-12-16 22:57:53 +0530

    prototype basics

---
+++ 11_OOP/object.js
@@ -0,0 +1,15 @@
+function multiplyBy5(num){
+    return num*5
+}
+
+multiplyBy5.power = 2;
+
+// console.log(multiplyBy5(5)); // 25
+// console.log(multiplyBy5.power); // 2
+// console.log(multiplyBy5.prototype); // {}
+
+
+function createUser(username,score){
+    this.username = username
+    this.score = score
+}
