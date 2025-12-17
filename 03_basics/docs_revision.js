ac29fe2c984697ee28dd3713e0caaebb40246f3a
ramanbhatia3 <bhattiramandeep3105@gmail.com>
2025-12-17 12:01:26 +0530

    docs

---
+++ 03_basics/docs_revision.js
@@ -0,0 +1,13 @@
+function myFunc(theObject) {
+  theObject.make = "Toyota";
+}
+
+const myCar = {
+  make: "Honda",
+  model: "Accord",
+  year: 1998,
+};
+
+console.log(myCar.make); // "Honda"
+myFunc(myCar);
+console.log(myCar.make); // "Toyota"
